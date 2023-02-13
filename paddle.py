865da4246e8b3c935136911f5612ac7be56f1c29
Gursehaj Singh <123921381+Gursehaj-Singh@users.noreply.github.com>
2023-02-12 17:55:31 -0700

    Create paddle.py

---
+++ paddle.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=1, stretch_len=10)
+        self.penup()
+        self.goto(0, -250)
+
+    def go_right(self):
+        new_x = self.xcor() + 40
+        self.goto(new_x, self.ycor())
+
+    def go_left(self):
+        new_x = self.xcor() - 40
+        self.goto(new_x, self.ycor())
